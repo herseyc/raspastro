7dd1fee0fe8d00eab9b7483a8797b7dd090f7c91
Hersey <herseyc@gmail.com>
2023-01-12 08:50:08 -0500

    Updates

--- install-indi-driver.sh
+++ install-indi-driver.sh
@@ -2,16 +2,18 @@
 
 BUILDDIR = build-pi
 
+mkdir -p ~/${BUILDDIR}
 echo "Enter the INDI 3rd-Party Package to Install: "
 read INDIPKG
 
 
-echo "Getting INDI 3rd-Party Drivers..."
+echo "Getting INDI 3rd-Party..."
+mkdir -p ~/${BUILDDIR}/Projects
 cd ~/${BUILDDIR}/Projects
 rm -rf indi-3rdparty
 git clone --depth=1 https://github.com/indilib/indi-3rdparty
 
-echo "Building INDI Lilbrary: "${INDIPKG}
+echo "Building INDI: "${INDIPKG}
 
 mkdir -p ~/${BUILDDIR}/Projects/build/${INDIPKG}
 cd ~/${BUILDDIR}/Projects/build/${INDIPKG}
