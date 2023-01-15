f5652cd29a7762b7dfefb72b0b4515879985c65a
Hersey <herseyc#gmail.com>
2023-01-15 14:58:10 -0500

    Added apt install of dependencies

--- install-kstars.sh
+++ install-kstars.sh
@@ -16,6 +16,24 @@ echo "Making Build Directory..."
 mkdir -p ~/${BUILDDIR}
 
 ############################################################
+# Install Dependencies
+############################################################
+sudo apt update
+sudo apt install libnova-dev libcfitsio-dev libusb-1.0-0-dev \
+        zlib1g-dev libgsl-dev build-essential cmake git \
+        libjpeg-dev libcurl4-gnutls-dev libtiff-dev libfftw3-dev \
+        libftdi-dev libgps-dev libraw-dev libdc1394-dev libgphoto2-dev \
+        libboost-dev libboost-regex-dev librtlsdr-dev liblimesuite-dev \
+        libftdi1-dev libavcodec-dev libavdevice-dev libeigen3-dev \
+        extra-cmake-modules libkf5plotting-dev libqt5svg5-dev \
+        libkf5xmlgui-dev libkf5kio-dev kinit-dev libkf5newstuff-dev \
+        libkf5doctools-dev libkf5notifications-dev \
+        qtdeclarative5-dev libkf5crash-dev gettext libkf5notifyconfig-dev \
+        wcslib-dev libqt5websockets5-dev xplanet xplanet-images \
+        qt5keychain-dev libsecret-1-dev breeze-icon-theme \
+        qml-module-qtquick-controls pkg-config libev-dev
+
+############################################################
 # Build and Install StellarSolver
 ############################################################
 echo "Building StellarSolver..."
