a9f5132f33511d54ffbcc0fd721c240c57f51903
Hersey <herseyc#gmail.com>
2023-01-20 14:29:45 -0500

    Created

---
+++ raspastro-status.py
@@ -0,0 +1,15 @@
+###############################################
+# Get the status of the RaspAstro Pi
+###############################################
+
+import netifaces as ni
+
+eth0ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
+print(f"LAN IP: {eth0ip}")  
+
+
+wlan0ip = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
+print(f"WAN IP: {wlan0ip}")  
+
+# Display GPS Fix
+print("GPS Fix")
