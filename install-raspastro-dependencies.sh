a0f2195b60c65ae817e9a0ff38758fb10ee13e97
Hersey <herseyc#gmail.com>
2023-02-01 19:52:47 -0500

    Updates

--- install-raspastro-dependencies.sh
+++ install-raspastro-dependencies.sh
@@ -29,7 +29,7 @@ sudo apt -y install libnova-dev libcfitsio-dev libusb-1.0-0-dev \
         qml-module-qtquick-controls pkg-config libev-dev \
         cdbs dkms  fxload libusb-dev libgsl0-dev  libkrb5-dev \
 	libtheora-dev libdc1394-22-dev libavcodec-dev libavdevice-dev \
-	libindi-dev libwxgtk3.0-gtk3-dev wx-common wx3.0-i18n libx11-dev
+	libindi-dev libwxgtk3.0-gtk3-dev wx-common wx3.0-i18n libx11-dev 
 
 echo "Installing python packages..."
 sudo pip3 install netifaces gps3
