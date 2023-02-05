2e3b60c55216bfd4625dff39a783a8ed039ff784
Hersey <herseyc#gmail.com>
2023-02-05 14:54:14 -0500

    ISS Next Passes

--- status/testing/iss.py
+++ status/testing/iss.py
@@ -2,17 +2,30 @@ from urllib import request
 import datetime as dt
 import ephem
 import math
+import predict
 
 degrees_per_radian = 180.0 / math.pi
 
 # Look here for hints on predicting ISS passes
 # https://stackoverflow.com/questions/52591629/pyephem-and-pypredict-gpredict-differences
+def passes(station, satellite, start=None, duration=7):
+    result = []
+    if start is not None:
+        station.date = ephem.date(start)
+    end = ephem.date(station.date + duration)
+    while station.date < end:
+        t_aos, azr, t_max, elt, t_los, azs = station.next_pass(satellite)
+        result.append({'aos': t_aos.datetime(), 'los': t_los.datetime()})
+        station.date = t_los + ephem.second
+    return result
+
 
 obslat = 36.779397335
 obslon = -76.535577396
 obs = ephem.Observer()
 obs.lon = obslon
 obs.lat = obslat
+obs.elev = 0
 obs.date = dt.datetime.utcnow()
 
 print(f"Date: {dt.datetime.utcnow()}")
@@ -22,26 +35,29 @@ iss_tledata = "https://celestrak.org/NORAD/elements/gp.php?CATNR=25544"
 f = request.urlopen(iss_tledata)
 iss = f.readlines()
 
-res = []
+# Format data so we can pass it to ephem
+tle = []
 for sub in iss:
     fix = str(sub)
     fix = fix.split("'")[1].split("'")[0] 
     fix = fix.replace("\\n", "")
     fix = fix.replace("\\r", "")
     fix = fix.strip()
-    res.append(fix)
+    tle.append(fix)
 
-iss_module_name = res[0]
-iss_tle1 = res[1]
-iss_tle2 = res[2]
-
-print(iss_module_name)
-print(iss_tle1)
-print(iss_tle2)
+# tle data for ephem
+iss_module_name = tle[0]
+iss_tle1 = tle[1]
+iss_tle2 = tle[2]
 
 iss_telemetry = ephem.readtle(iss_module_name, iss_tle1, iss_tle2)
-
 iss_telemetry.compute(obs)
+
+epoch = dt.datetime.utcnow()
+for i in passes(obs, iss_telemetry, epoch, 5):
+    print(i['aos'], i['los'])
+
+
 iss_range = iss_telemetry.range
 iss_alt = iss_telemetry.alt
 iss_az = iss_telemetry.az
@@ -57,15 +73,5 @@ print(f"ISS Geocentric Lon: {iss_sublong}")
 print(f"ISS Elevation: {round(iss_elevation / 1609, 2)} Miles")
 print(f"ISS RA: {iss_telemetry.ra}")
 print(f"ISS DEC: {iss_telemetry.dec}")
-
-print("Next ISS Pass")
-iss_next_pass = obs.next_pass(iss_telemetry)
-print(f"Rise Time: {iss_next_pass[0]}")
-print(f"Rise Azimuth: {iss_next_pass[1]}")
-print(f"Max Altitude Time: {iss_next_pass[2]}")
-print(f"Max Altitude: {iss_next_pass[3]}")
-print(f"Set Time: {iss_next_pass[4]}")
-print(f"Set Azimuth {iss_next_pass[5]}")
-
 
 
