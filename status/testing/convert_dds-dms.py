c2505e265531d2a07706a7a9a80868180248fab3
Hersey <herseyc#gmail.com>
2023-02-07 14:32:12 -0500

    Converted to function

--- status/testing/convert_dds-dms.py
+++ status/testing/convert_dds-dms.py
@@ -2,15 +2,13 @@
 MY_LAT = 36.7794571 # Your latitude
 MY_LON = -76.5355366 # Your longitude
 
-list = [MY_LAT, MY_LON]
-
-for dd in list:
-    print(dd)
+def convert_ddstodms(dd):
     degrees = int(dd)
     temp = 60 * (dd - degrees)
     minutes = int(temp)
     seconds = round(60 * (temp - minutes), 1)
     dms = f"{degrees}:{abs(minutes)}:{abs(seconds)}"
-    print(dms)
+    return dms
 
-
+print(convert_ddstodms(MY_LAT))
+print(convert_ddstodms(MY_LON))
