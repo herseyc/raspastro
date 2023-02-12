88221ac9753faad9b70d3b9418521520dec45eb4
Hersey <herseyc#gmail.com>
2023-02-12 09:06:31 -0500

    cleanup, comments, and function to convert DMS to decimal degrees

--- status/rasp_calc_func.py
+++ status/rasp_calc_func.py
@@ -19,6 +19,7 @@ def to_local(time):
     local_time = time.astimezone(to_zone)
     return local_time
 
+
 ########################################
 # function to convert Meters to Miles  #
 ########################################
@@ -30,46 +31,10 @@ def meters_to_miles(meters):
     miles = round(meters / 1609, 2)
     return miles
 
-###################################################
-# function to check if UTC is a night local time  #
-###################################################
-def is_at_night(checkdate, lat, lon):
-    '''
-    determine if UTC (checkdate) for location (lat, lon) is daytime
-    or nighttime in local time based on Sunrise-Sunset.org API
-    return True if nighttime, False if daytime
-    '''
-    #Get sunrise/sunset for UTC checkdate from Sunrise-Sunset.org API
-    fordate = f"{checkdate.year}-{checkdate.month}-{checkdate.day}"
-    # Set up parameters for Sunrise-Sunset.org API Request
-    parameters = {
-        "lat": lat,
-        "lng": lon,
-        "formatted": 0,
-        "date": fordate
-    }
 
-    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-    response.raise_for_status()
-    data = response.json()
-    # Convert sunrise and sunset time from UTC to local time
-    # and get minutes since midnight
-    local_sunrise = to_local(datetime.fromisoformat(data["results"]["sunrise"]))
-    local_sunrise_min = (local_sunrise.hour * 60) + local_sunrise.minute
-    local_sunset = to_local(datetime.fromisoformat(data["results"]["sunset"]))
-    local_sunset_min = (local_sunset.hour * 60) + local_sunset.minute
-
-    # convert UTC to local time
-    local_checkdate = to_local(checkdate)
-    # get local minutes since midnight
-    local_checkdate_min = (local_checkdate.hour * 60) + local_checkdate.minute
-
-    if  local_sunset_min > local_checkdate_min  > local_sunrise_min:
-        return False
-    else:
-        return True
-
-
+######################################################
+# Take a datetime and make it more "human" readable  #
+######################################################
 def time_to_human(time):
      day = time.day
      month = time.month
@@ -94,6 +59,10 @@ def time_to_human(time):
      humantime = f"{month}/{day}/{year} {hour}:{minute} {tod} {tz}"
      return humantime
 
+
+####################################
+# convert decimal degrees to D:M:S #
+####################################
 def convert_dd_to_dms(dd):
     degrees = int(dd)
     temp = 60 * (dd - degrees)
@@ -101,4 +70,58 @@ def convert_dd_to_dms(dd):
     seconds = round(60 * (temp - minutes), 1)
     dms = f"{degrees}:{abs(minutes)}:{abs(seconds)}"
     return dms
- 
+
+
+###################################
+# convert D:M:S to Decimal Degree #
+###################################
+def convert_dms_to_dd(dms):
+    dms_list = str(dms).split(":")
+    if dms_list[0] == '-':
+       dd = float(dms_list[0] - float(dms_list[1])/60 - float(dms_list[2])/3600
+    else:
+       dd = float(dms_list[0] + float(dms_list[1])/60 + float(dms_list[2])/3600
+    return dd
+
+
+# NO LONGER USED # 
+###################################################
+# function to check if UTC is a night local time  #
+# Uses sunrise-sunset.org API - works but slow    #
+###################################################
+#def is_at_night(checkdate, lat, lon):
+#    '''
+#    determine if UTC (checkdate) for location (lat, lon) is daytime
+#    or nighttime in local time based on Sunrise-Sunset.org API
+#    return True if nighttime, False if daytime
+#    '''
+#    #Get sunrise/sunset for UTC checkdate from Sunrise-Sunset.org API
+#    fordate = f"{checkdate.year}-{checkdate.month}-{checkdate.day}"
+#    # Set up parameters for Sunrise-Sunset.org API Request
+#    parameters = {
+#        "lat": lat,
+#        "lng": lon,
+#        "formatted": 0,
+#        "date": fordate
+#    }
+#
+#    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+#    response.raise_for_status()
+#    data = response.json()
+#    # Convert sunrise and sunset time from UTC to local time
+#    # and get minutes since midnight
+#    local_sunrise = to_local(datetime.fromisoformat(data["results"]["sunrise"]))
+#    local_sunrise_min = (local_sunrise.hour * 60) + local_sunrise.minute
+#    local_sunset = to_local(datetime.fromisoformat(data["results"]["sunset"]))
+#    local_sunset_min = (local_sunset.hour * 60) + local_sunset.minute
+#
+#    # convert UTC to local time
+#    local_checkdate = to_local(checkdate)
+#    # get local minutes since midnight
+#    local_checkdate_min = (local_checkdate.hour * 60) + local_checkdate.minute
+#
+#    if  local_sunset_min > local_checkdate_min  > local_sunrise_min:
+#        return False
+#    else:
+#        return True
+
