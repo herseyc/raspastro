e38e1c57fc0847222b11d2531ffa417699d683a0
Hersey <herseyc#gmail.com>
2023-02-12 13:29:46 -0500

    Created config.py to store user variables

---
+++ status/config.py
@@ -0,0 +1,20 @@
+# Store RaspAstro Configuration Variables
+
+# Number of days to calculate ISS Passes
+PASSDAYS = 5
+
+# INDI Web Manager API Endpoint
+INDIWEBMANAGER_API_ENDPOINT = "http://localhost:8624"
+
+
+# Work in Progress - Placeholders for now
+# Set to True to use GPS, False to use user configured lat/lon/elev
+USE_GPS = True
+
+# If USE_GPS is False use the following for observer location
+# Latitude and Longitude DMS string
+MY_LAT = "36:46:46.0"
+MY_LON = "-76:32:8.1"
+
+# Observer Elevation (float in meters)
+MY_ELEVATION = 7.7
