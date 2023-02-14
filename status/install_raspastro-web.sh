15fb0db013cb23bb17f6d9e8ee9c067736e0c7ff
Hersey <herseyc#gmail.com>
2023-02-14 13:02:15 -0500

    Install script and systemctl service script.

--- status/install_raspastro-web.sh
+++ status/install_raspastro-web.sh
@@ -3,7 +3,7 @@
 # Installs RaspAstro Web to the INSTALL_DIR #
 #############################################
 
-INSTALL_DIR=/var/www/raspastro
+INSTALL_DIR="/var/www/raspastro"
 
 echo "Installs RaspAstro Web to ${INSTALL_DIR}"
 current_user="$(id -u -n)"
@@ -20,16 +20,19 @@ chmod +x ${INSTALL_DIR}/raspastro-web.py
 # Add service config to start automatically
 echo "Setting up RaspAstro Web to start as current user ${current_user}..."
 sed -i 's/^User=.*/User='${current_user}'/' ${INSTALL_DIR}/raspastroweb.service
-#sudo cp ${INSTALL_DIR}/raspastroweb.service /etc/systemd/system
-#sudo chmod 644 /etc/systemd/system/raspastroweb.service
-#sudo sytemctl daemon-reload
-#sudo systemctl enable raspastroweb.service
-#sudo systemctl start raspastroweb.service
 
+### NEED TO SET INSTALL_DIR IN raspastroweb.service 
+## Right now it is set to /var/www/raspastro
+# if you use a different directory you will need to manually update.
+
+sudo cp ${INSTALL_DIR}/raspastroweb.service /etc/systemd/system
+sudo chmod 644 /etc/systemd/system/raspastroweb.service
+sudo sytemctl daemon-reload
+sudo systemctl enable raspastroweb.service
+sudo systemctl start raspastroweb.service
 
 
 echo "RaspAstro Web Installed!"
-#echo "To start RaspAstro Web"
-#echo "Change to ${INSTALL_DIR}: cd ${INSTALL_DIR}"
-#echo "Run RaspAstro Web:  ./raspastro-web.py"
+echo "To check RaspAstro Web service status"
+echo "Run:  sudo systemctl status raspastroweb.service"
 
