8e845292240f36f45154527a44a9f825f3461519
Hersey <herseyc#gmail.com>
2023-02-17 09:08:24 -0500

    Save off config.py to config.old

--- RaspAstroWeb/install_raspastro-web.sh
+++ RaspAstroWeb/install_raspastro-web.sh
@@ -5,32 +5,26 @@
 
 INSTALL_DIR="/var/www/raspastro"
 
-echo "Installs RaspAstro Web to ${INSTALL_DIR}"
+echo "Installing RaspAstro Web to ${INSTALL_DIR}"
 current_user="$(id -u -n)"
 sudo mkdir -p ${INSTALL_DIR}
 sudo chown ${current_user} ${INSTALL_DIR}
 
+if [ -f "${INSTALL_DIR}/config.py" ]; then
+    echo "Found config.py in ${INSTALL_DIR}. Saving existing config.py to config.old"
+    cp ${INSTALL_DIR}/config.py ${INSTALL_DIR}/config.old
+fi
 
+echo "Copying python files to ${INSTALL_DIR}"
 cp *.py ${INSTALL_DIR}
+echo "Copying template directory to ${INSTALL_DIR}"
 cp -R templates ${INSTALL_DIR}
+echo "Copying static directory to ${INSTALL_DIR}"
 cp -R static ${INSTALL_DIR}
+echo "Copying supporing files to ${INSTALL_DIR}"
 cp raspastroweb.service ${INSTALL_DIR}
+echo "Making  ${INSTALL_DIR}/raspastro-web.py executable."
 chmod +x ${INSTALL_DIR}/raspastro-web.py
 
-# Add service config to start automatically
-#echo "Setting up RaspAstro Web to start as current user ${current_user}..."
-#sed -i 's/^User=.*/User='${current_user}'/' ${INSTALL_DIR}/raspastroweb.service
-### NEED TO SET INSTALL_DIR IN raspastroweb.service 
-## Right now it is set to /var/www/raspastro
-# if you use a different directory you will need to manually update.
-#sudo cp ${INSTALL_DIR}/raspastroweb.service /etc/systemd/system
-#sudo chmod 644 /etc/systemd/system/raspastroweb.service
-#sudo sytemctl daemon-reload
-#sudo systemctl enable raspastroweb.service
-#sudo systemctl start raspastroweb.service
-
-
-echo "RaspAstro Web Installed!"
-#echo "To check RaspAstro Web service status"
-#echo "Run:  sudo systemctl status raspastroweb.service"
+echo "RaspAstro Web Installed in ${INSTALL_DIR}!"
 
